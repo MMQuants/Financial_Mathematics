01fb5288336d87847042b95c2bd6ca40fea4af72
mmquants <mar4o86@mail.bg>
2022-11-24 11:50:03 +0200

    equation

--- Euler.m
+++ Euler.m
@@ -7,8 +7,23 @@ b=2;
 hold on; 
 grid on;
 
-y = dsolve('Du= -u*an(x)+ cos(x)^2')
+y = dsolve('Du= -u*tan(x)+ cos(x)^2', 'u(0)=1', 'x');
+x = linspace(0,2,200);
+plot(x, eval(y), 'k');
 
+x= [];
+y=[];
+h = 0.5;
+
+x=0:h:2;
+y[1] = y0;
+
+    for i=1:n-1
+        y(i+1) = (1-h*tan(x(i))) * y(i) + h*cos(x(i))^2;
+    end
+
+x
+y
 
 
 end
