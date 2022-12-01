2da20783ff1437c521d9c4efc2f85a072af24d65
mmquants <mar4o86@mail.bg>
2022-12-01 11:09:54 +0200

    ploting

--- Euler.m
+++ Euler.m
@@ -1,29 +1,26 @@
 function Euler
 
-x0=0;
-y0=1;
-b=2;
+x0 = 0;
+y0 = 1;
+b = 2;
 
-hold on; 
+hold on;
 grid on;
 
-y = dsolve('Du= -u*tan(x)+ cos(x)^2', 'u(0)=1', 'x');
-x = linspace(0,2,200);
+y = dsolve('Du = -u*tan(x)+cos(x)^2', 'u(0)=1', 'x');
+x = linspace(0, 2, 300);
 plot(x, eval(y), 'k');
 
-x= [];
-y=[];
-h = 0.5;
+x = [];
+y = [];
+h = 0.01;
 
-x=0:h:2;
-y[1] = y0;
+x = 0:h:2;
+y(1) = y0;
 
-    for i=1:n-1
-        y(i+1) = (1-h*tan(x(i))) * y(i) + h*cos(x(i))^2;
+    for i=1:length(x)-1
+        y(i+1) = (1-h*tan(x(i))) * y(i) + h * cos(x(i))^2;
     end
 
-x
-y
-
-
+    plot(x, y, 'r.');
 end
