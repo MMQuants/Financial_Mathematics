992d9546bbccc85aede9773f3a6df31a2f7427fd
mmquants <mar4o86@mail.bg>
2022-12-01 12:02:50 +0200

    fixing errors

--- SecondOrd.m
+++ SecondOrd.m
@@ -3,11 +3,14 @@ function SecondOrd
 x0 = 0;
 b=20;
 u0=3;
-v0=0'
+v0=0;
+
+
 
 x = linspace(x0, b, 300);
 
 u = dsolve('D2u = -4*u-(1/2)*Du', 'u(0)=u0', 'Du(0)=v0', "x");
+
 
 plot(x, eval(u),"k")
 
@@ -19,13 +22,17 @@ y=[];
 y(1)=u0;
 y(2) = u0+h*v0+h^2/2*f(x0,u0,v0);
 
+hold on;
+grid on;
+
+
 for i = 2:length(x)-1
-    y(i+1) = 2*y(i)-y(i-1)+h^2*f(x(i),y(i),(y(i)-y(i-1)));
+    y(i+1) = 2*y(i)-y(i-1)+h^2*f(x(i),y(i),(y(i)-y(i-1)/h));
 
 end
 
     function z = f(p,q,t)
-        z = -4*q-1/2*t
+        z = -4*q-1/2*t;
 
 
     end
