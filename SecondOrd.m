f7ee44333253874c81078220c71b5c1c2e4445f1
mmquants <mar4o86@mail.bg>
2022-12-01 11:59:45 +0200

    plotting the solution

--- SecondOrd.m
+++ SecondOrd.m
@@ -7,10 +7,28 @@ v0=0'
 
 x = linspace(x0, b, 300);
 
-u = dsolve('D2u = -4*u-1/2*Du', 'u(0)=3', 'Du(0)=0', "x");
-u
+u = dsolve('D2u = -4*u-(1/2)*Du', 'u(0)=u0', 'Du(0)=v0', "x");
+
+plot(x, eval(u),"k")
+
+h=0.5;
+x=[];
+x = x0:h:b;
+
+y=[];
+y(1)=u0;
+y(2) = u0+h*v0+h^2/2*f(x0,u0,v0);
+
+for i = 2:length(x)-1
+    y(i+1) = 2*y(i)-y(i-1)+h^2*f(x(i),y(i),(y(i)-y(i-1)));
+
+end
+
+    function z = f(p,q,t)
+        z = -4*q-1/2*t
 
 
+    end
 
 
 
