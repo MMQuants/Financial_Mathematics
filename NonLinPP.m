80885d5c9dcdf87b68ba17e68bfe20846ed2dd4d
mmquants <mar4o86@mail.bg>
2023-01-19 11:54:12 +0200

    plotting

--- NonLinPP.m
+++ NonLinPP.m
@@ -1,7 +1,24 @@
 function NonLinPP
 
+tmax =10;
 k = 1;
 ak = (-1)^k;
 bk = 0;
+
+axis([ak-5;ak+5; bk-5;bk+5]);
+
+plot(ak, bk, 'r');
+
+
+x = ak-4:1:ak+4;
+y = bk-4:1:bk+4;
+
+[X,Y] = meshgrid(x,y);
+DX = Y;
+DY = sin(X+Y);
+D = sqrt(DX.^2 + DY.^2);
+
+quiver(X,Y,DX./D, DY./D, 'g')
+
 
 end
