aca5e492e4ec01c87f56bec37fdac5f5f73bc7c5
mmquants <mar4o86@mail.bg>
2023-01-19 11:23:11 +0200

    plotting

--- PhaseMovie.m
+++ PhaseMovie.m
@@ -1,10 +1,16 @@
 function PhaseMovie
 
+% inputs
     tmax = 10;
     a=1;
     b = 0.3;
     c=1;
     d =0.2;
+    
+    axis([-1, c/d+15, -1, a/b+10]);
+    plot(0,0,'r*', c/d, a/b, 'r*')
+
+
 
 
 end
