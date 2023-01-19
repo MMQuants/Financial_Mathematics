3108bf4c505b6324aeeba1bfc8d49d72fbf48a48
mmquants <mar4o86@mail.bg>
2023-01-19 12:07:09 +0200

    fixing bugs

--- NonLinPP.m
+++ NonLinPP.m
@@ -10,7 +10,6 @@ hold on;
 grid on;
 plot(ak, bk, 'r');
 
-
 x = ak-4:1:ak+4;
 y = bk-4:1:bk+4;
 
@@ -25,18 +24,18 @@ for i = 1:length(x)
 
     for j = 1: length(y)
         [T1, Z1] = ode45(@ff, [0,tmax], [X(i,j), Y(i,j)]);
-        [T2, Z2] = ode45(@ff, [0,tmax], [X(i,j), Y(i,j)]);
+        [T2, Z2] = ode45(@ff, [0,-tmax], [X(i,j), Y(i,j)]);
+
+        plot(Z1(:,1), Z1(:,2), 'r');
+        plot(Z2(:,1), Z2(:,2), 'b');
     end
 end
-
 
     function z = ff(t,v)
 
         z = [v(2); sin(v(1)+v(2))];
 
     end
-
-
 
 
 end
